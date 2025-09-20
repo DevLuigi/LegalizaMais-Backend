aad35df7350a949326b3e35fcce4862366d5cc8b
DevLuigi <luigidasilvacoelho@gmail.com>
2025-09-19 22:53:09 -0300

    Feat: Adiciona estrutura do projeto

---
+++ src/main/java/_bits/LegalizaMais/LegalizaMaisApplication.java
@@ -0,0 +1,13 @@
+package _bits.LegalizaMais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalizaMaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegalizaMaisApplication.class, args);
+	}
+
+}
